03a4ccab8933d6e59bb86b0336382baee02a2b69
Konstantinov Andrey <andrey.konstantinov1992@gmail.com>
2019-10-28 12:29:14 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
+    icons: string[];
+    backgrounds: string[];
 }
 
-export default App;
+interface State {
+    selectedIcons: string[];
+    selectedBackground: string;
+}
+
+const initialState: State = {
+    selectedIcons: [],
+    selectedBackground: 'transparent',
+};
+
+const AppContext = React.createContext(initialState);
+
+class App extends React.Component<Props, State> {
+    state = {
+        ...initialState,
+    };
+
+    render() {
+        return (
+            <AppContext.Provider value={this.state}>
+                {this.props.children}
+            </AppContext.Provider>
+        )
+    }
+}
+
+export default App
+
+
