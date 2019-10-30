3b9db53bd1282712ce6253f817b2bca813285521
Konstantinov Andrey <andrey.konstantinov1992@gmail.com>
2019-10-30 17:32:59 +0200

    add readme

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import pkg from "./package.json";
 
 export default [
     {
-        input: "src/index.ts",
+        input: "src/module.ts",
         external: Object.keys(pkg.peerDependencies || {}),
         plugins: [
             resolve({browser: true}),
