29adb44d08e768b698322441e7a43db8701d7e07
Vladimir Yavdoshchuk <yavdoshchuk.vladimir@steelkiwi.com>
2019-10-30 12:08:08 +0200

    Change package name

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,20 @@ import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
 
 export default [
-    {
-        input: "src/index.ts",
-        external: Object.keys(pkg.peerDependencies || {}),
-        plugins: [
-            typescript({
-                typescript: require("typescript")
-            })
-        ],
-        output: [
-            {
-                file: "index.js",
-                format: "es",
-                banner: "/* eslint-disable */"
-            }
-        ]
-    }
+  {
+    input: "src/index.ts",
+    external: Object.keys(pkg.peerDependencies || {}),
+    plugins: [
+      typescript({
+        typescript: require("typescript")
+      })
+    ],
+    output: [
+      {
+        file: "index.js",
+        format: "es",
+        banner: "/* eslint-disable */"
+      }
+    ]
+  }
 ];
