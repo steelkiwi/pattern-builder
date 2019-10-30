de3662268fdf9b284de40249c5e9146750b51ac7
Vladimir Yavdoshchuk <yavdoshchuk.vladimir@steelkiwi.com>
2019-10-30 12:26:49 +0200

    Refactor App

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,64 @@
 import React, {Component, createRef} from 'react';
-import {initialState, AppContext} from "./store";
 import {Color} from "react-color";
+
+import {AppContext} from "./store";
+import {getRGBAString} from "./helpers";
 import './index.scss'
-import {getRGBAString} from "./helpers/helpers";
 
 interface Props {
-    icons: string[];
-    backgrounds?: string[];
-    onDownload?: (img: string) => void;
+  icons: string[];
+  backgrounds?: string[];
+  onDownload?: (img: string) => void;
 }
 
 interface State {
-    patternIcons: string[];
-    selectedBackground: Color;
+  patternIcons: string[];
+  selectedBackground: Color;
 }
 
+export default class App extends Component<Props, State> {
+  state = {
+    patternIcons: [],
+    selectedBackground: 'green',
+  };
 
-class App extends Component<Props, State> {
-    state = {
-        patternIcons: [],
-        selectedBackground: 'green',
-    };
+  canvasRef = createRef<HTMLDivElement>();
 
-    canvasRef = createRef<HTMLDivElement>();
+  addPatternIcon = (icon: string) => {
+    this.setState(state => (
+      {
+        ...state,
+        patternIcons: [...state.patternIcons, icon]
+      }))
+  };
 
-    addPatternIcon = (icon: string) => {
-        this.setState(state => (
-            {
-                ...state,
-                patternIcons: [...state.patternIcons, icon]
-            }))
-    };
+  changeBackground = (background: any) => {
+    const {rgb} = background;
+    this.setState({selectedBackground: getRGBAString(rgb)})
+  };
 
-    changeBackground = (background: any) => {
-        const {rgb} = background;
-        this.setState({selectedBackground: getRGBAString(rgb)})
-    };
-
-
-
-    render() {
-        const { onDownload } = this.props;
-        return (
-            <AppContext.Provider value={
-                {
-                    state: this.state,
-                    icons: this.props.icons || initialState.icons,
-                    backgrounds: this.props.backgrounds || initialState.backgrounds,
-                    canvasRef: this.canvasRef,
-                    handlers: {
-                        addPatternIcon: this.addPatternIcon,
-                        changeBackground: this.changeBackground,
-                    },
-                    callbacks: {
-                        onDownload
-                    }
-                }
-            }>
-                <div className="pb__wrapper">
-                    {this.props.children}
-                </div>
-            </AppContext.Provider>
-        )
-    }
+  render() {
+    const {onDownload, icons, backgrounds, children} = this.props;
+    return (
+      <AppContext.Provider value={
+        {
+          state: this.state,
+          icons: icons,
+          backgrounds: backgrounds,
+          canvasRef: this.canvasRef,
+          handlers: {
+            addPatternIcon: this.addPatternIcon,
+            changeBackground: this.changeBackground,
+          },
+          callbacks: {
+            onDownload
+          }
+        }
+      }>
+        <div className="pb__wrapper">
+          {children}
+        </div>
+      </AppContext.Provider>
+    )
+  }
 }
-
-export default App
-
-
